0dd44364aec932062250028bdb8e9ba8d48ca833
Chet Hill <chetdhill@gmail.com>
2019-04-26 16:29:48 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule
